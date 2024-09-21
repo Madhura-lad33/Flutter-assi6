b52e75dcbe55a09dd870949d50622723725562c9
Madhura-lad33 <madhuralad28@gmail.com>
2024-09-21 16:47:31 +0530

    Add files via upload

---
+++ whileloop12.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  int num = int.parse(stdin.readLineSync()!);
+  if (num % 2 == 1) {
+    while(num > 0) {
+      print(num);
+      num = num - 2;
+    }
+  } else {
+    while(num > 0) {
+      print (num);
+      num--;
+    }
+  }
+}
